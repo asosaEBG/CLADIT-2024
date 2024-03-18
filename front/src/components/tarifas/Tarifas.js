7ff847fbed122d4c9cfb3b8a49aaa0a394a33911
asosaEBG <asosa@ebg.edu.gt>
2024-03-18 10:11:59 -0600

    [UPDATE] changes on tarifas

--- front/src/components/tarifas/Tarifas.js
+++ front/src/components/tarifas/Tarifas.js
@@ -1,44 +1,48 @@
 import React from "react";
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
-import { Typography } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 const Tarifas = () => {
   return (
     <Box>
-      <Stack spacing={3} style={{ padding: "5%" }}>
-        <Typography
-          variant="h2"
-          style={{ color: "#1e3d52", textAlign: "center" }}
-        >
-          EL REGISTRO INCLUYE
-        </Typography>
-        <Typography
-          variant="p"
-          style={{ fontSize: "1.5em", textAlign: "justify" }}
-        >
-          <ul>
-            <li>Conferencias</li>
-            <li>Taller precongreso</li>
-            <li>Refrigerios a.m. y p.m.</li>
-            <li>Almuerzos</li>
-            <li>Acceso al área de stands</li>
-            <li>Certificado de participación digital</li>
-            <li>Tarifa especial a parqueo</li>
-            <li>Grabación de conferencias</li>
-          </ul>
-        </Typography>
-        <Typography
-          variant="h2"
-          style={{ color: "#1e3d52", textAlign: "center" }}
-        >
-          TARIFAS DE INSCRIPCIÓN
-        </Typography>
-        <img
-          src="https://escuela-bancaria.s3.us-east-2.amazonaws.com/tarifas+CLADIT.png"
-          loading="lazy"
-          style={{ width: "100%" }}
-        />
-      </Stack>
+      <Grid container justifyContent="center" p={5}>
+        <Grid xs={12} md={12} lg={6} textAlign="center">
+          <Typography
+            variant="h4"
+            style={{ color: "#1e3d52", textAlign: "center" }}
+          >
+            TARIFAS DE INSCRIPCIÓN
+          </Typography>
+          <img
+            src="https://escuela-bancaria.s3.us-east-2.amazonaws.com/tarifas+CLADIT.png"
+            loading="lazy"
+            style={{ width: "100%" }}
+          />
+        </Grid>
+        <Grid xs={12} md={12} lg={6}>
+          <Typography
+            variant="h4"
+            style={{ color: "#1e3d52", textAlign: "center" }}
+          >
+            EL REGISTRO INCLUYE
+          </Typography>
+          <Typography
+            variant="p"
+            style={{ fontSize: "1em", textAlign: "justify" }}
+          >
+            <ul>
+              <li>Conferencias</li>
+              <li>Taller precongreso</li>
+              <li>Refrigerios a.m. y p.m.</li>
+              <li>Almuerzos</li>
+              <li>Acceso al área de stands</li>
+              <li>Certificado de participación digital</li>
+              <li>Tarifa especial a parqueo</li>
+              <li>Grabación de conferencias</li>
+            </ul>
+          </Typography>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
