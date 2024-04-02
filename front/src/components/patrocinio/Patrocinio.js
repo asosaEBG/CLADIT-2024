5f52d6e4d4aa2614d5e4478d62afb5670b9329d2
asosaEBG <asosa@ebg.edu.gt>
2024-04-02 11:31:01 -0600

    [UPDATE] few things

--- front/src/components/patrocinio/Patrocinio.js
+++ front/src/components/patrocinio/Patrocinio.js
@@ -12,7 +12,7 @@ const Patrocinio = () => {
           PLANOS STAND
         </Typography>
         <img
-          src="https://escuela-bancaria.s3.us-east-2.amazonaws.com/WhatsApp+Image+2024-03-21+at+14.59.28.jpeg"
+          src="https://escuela-bancaria.s3.us-east-2.amazonaws.com/1712078757472-2%20Plano%20CLADIT%202024.jpg"
           style={{ width: "100%" }}
           loading="lazy"
         />
