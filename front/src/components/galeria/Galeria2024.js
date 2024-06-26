a5db4c615ea302da1dd5eed1d69275491f23d85f
asosaEBG <asosa@ebg.edu.gt>
2024-06-26 12:23:44 -0600

    [UPDATE] galeria cladit

--- front/src/components/galeria/Galeria2024.js
+++ front/src/components/galeria/Galeria2024.js
@@ -5,6 +5,7 @@ import { Typography, Grid } from "@mui/material";
 import ImageList from "@mui/material/ImageList";
 import ImageListItem from "@mui/material/ImageListItem";
 const s3Helper = require("../../helpers/S3.controller");
+const helpers = require("../../helpers/helpers");
 const Galeria2024 = () => {
   const [contador] = useState(0);
   const [files, setFiles] = useState([]);
@@ -12,7 +13,7 @@ const Galeria2024 = () => {
     s3Helper
       .listFolderFilesUrl("GALERIA-CLADIT-2024")
       .then((files) => {
-        setFiles(files);
+        setFiles(helpers.shuffleArray(files));
       })
       .catch((error) => {
         console.log(error);
@@ -56,13 +57,14 @@ const Galeria2024 = () => {
             </video>
           </Grid>
         </Grid>
-        <ImageList cols={2}>
+        <ImageList cols={2} variant="woven">
           {files.map((actual, index) => (
             <ImageListItem>
               <img
                 src={actual}
                 alt={`img-galeria-cladit-2024-${index}`}
                 loading="lazy"
+                style = {{objectFit:'contain'}}
               />{" "}
             </ImageListItem>
           ))}
