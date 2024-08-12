6ab116efe027308cfcf8dd86cca3cbda6b9abe94
asosaEBG <asosa@ebg.edu.gt>
2024-08-12 09:19:53 -0600

    [UPDATE] tarifas

--- front/src/components/tarifas/TarifasQZ.js
+++ front/src/components/tarifas/TarifasQZ.js
@@ -5,7 +5,7 @@ const TarifasQZ = () => {
   return (
     <Box style={{ display: "flex", justifyContent: "center" }}>
       <img
-        src="https://escuela-bancaria.s3.us-east-2.amazonaws.com/8e41c529-c379-46c8-b274-31510020185a.png"
+        src="https://escuela-bancaria.s3.us-east-2.amazonaws.com/a05b93a1-d35a-40ac-bfb5-92e9ef553813.png"
         loading="lazy"
         style={{ width: "50%" }}
       />
